54fb96b4e7abb5580c4d558ea6a83606a390260c
Dobri Dimov Dobrev <126101328+dobridimovdobrev@users.noreply.github.com>
2025-10-03 21:43:27 +0200

    ok

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,7 +19,7 @@ function Carousel(props) {
           768: { slidesPerView: 4, spaceBetween: 10 },
           992: { slidesPerView: 5, spaceBetween: 10 },
           1200: { slidesPerView: 6, spaceBetween: 10 },
-          /* importante mettere questi breakpoints perche lo spazio vuoto si crea solo per screen grandi oltre 1 */
+          /* importante mettere questi breakpoints perche lo spazio vuoto si crea solo per screen grandi oltre 1600px */
           1600: { slidesPerView: 6, spaceBetween: 10 },
           1920: { slidesPerView: 8, spaceBetween: 10 },
           
@@ -49,44 +49,3 @@ function Carousel(props) {
 }
 
 export default Carousel;
-
-
-/* import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-import './styles.css';
-
-// import required modules
-import { Pagination } from 'swiper/modules';
-
-export default function App() {
-  return (
-    <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
-  );
-}
- */
